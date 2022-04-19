9179d2d8af9d25e2de4514c379da16938b82311b
Muhammed Shabeer <androlite4@gmail.com>
2022-04-19 16:45:56 +0530

    Added Second Frame

--- lib/MainScreen.dart
+++ lib/MainScreen.dart
@@ -19,72 +19,139 @@ class _MainScreenState extends State<MainScreen> {
     Column(
       children: [
         Expanded(
-            child: FirebaseAnimatedList(
-                duration: Duration(milliseconds: 100),
-                query: database.reference().child("sunrise"),
-                itemBuilder: (BuildContext context, DataSnapshot snapshot,
-                        Animation<double> animation, int index) =>
-                    Card(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+          child: FirebaseAnimatedList(
+            duration: Duration(milliseconds: 100),
+            query: database.reference().child("sunrise"),
+            itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                    Animation<double> animation, int index) =>
+                Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              shadowColor: Colors.black,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  FadeInImage.assetNetwork(
+                      fadeInDuration: Duration(milliseconds: 100),
+                      fadeOutDuration: Duration(milliseconds: 100),
+                      placeholder: "assets/images/splash.png",
+                      imageCacheHeight: 1000,
+                      imageCacheWidth: 1000,
+                      image: snapshot.child("img").value.toString()),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          snapshot.child("title").value.toString(),
+                          textAlign: TextAlign.left,
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
                       ),
-                      shadowColor: Colors.black,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          FadeInImage.assetNetwork(
-                              fadeInDuration: Duration(milliseconds: 100),
-                              fadeOutDuration: Duration(milliseconds: 100),
-                              placeholder: "assets/images/splash.png",
-                              imageCacheHeight: 1000,
-                              imageCacheWidth: 1000,
-                              image: snapshot.child("img").value.toString()),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              snapshot.child("title").value.toString(),
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                            child: Text(
-                              snapshot.child("desc").value.toString(),
-                              textAlign: TextAlign.left,
-                              style: TextStyle(fontSize: 15,),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Container(
-                            alignment: Alignment.centerLeft,
-                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                            child: Text(
-                              snapshot.child("school").value.toString(),
-                              textAlign: TextAlign.left,
-                              style: TextStyle(fontSize: 11, color: Colors.grey),
-                            ),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                        ],
+                      Spacer(),
+                      Container(
+                        padding: EdgeInsets.only(right: 10.0),
+                        alignment: Alignment.center,
+                        child: Icon(
+                          Icons.more_vert,
+                          size: 20,
+                        ),
                       ),
-                    )))
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Text(
+                      snapshot.child("desc").value.toString(),
+                      textAlign: TextAlign.left,
+                      style: TextStyle(
+                        fontSize: 15,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                    child: Text(
+                      snapshot.child("school").value.toString(),
+                      textAlign: TextAlign.left,
+                      style: TextStyle(fontSize: 11, color: Colors.grey),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
       ],
     ),
-    Text(
-      'Index 1: Business',
+    Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Card(
+          child: Row(
+            children: [
+              Container(
+                child: Image(
+                  image: AssetImage("assets/images/logo.png"),
+                  height: 50,
+                  width: 50,
+                ),
+              ),
+              Spacer(),
+              Container(
+                padding: EdgeInsets.only(left: 10.0, right: 10.0),
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Your Profile",
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Spacer(),
+              Container(
+                alignment: Alignment.center,
+                child: PopupMenuButton(
+                  icon: Icon(Icons.more_vert),
+                  itemBuilder: (context) => [
+                    PopupMenuItem(
+                      child: Text("Login"),
+                      value: 1,
+                    ),
+                    PopupMenuItem(
+                      child: Text("shabeer"),
+                      value: 2,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        Card(
+          child: Column(
+            children: [
+              Text("Upload"),
+
+            ],
+          ),
+        )
+      ],
     ),
   ];
 
